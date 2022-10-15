df57106283caa3a1ac9e9d3f3c0b8dc8bc2cd79f
Amilcar Munoz <yellowballpython@gmail.com>
2022-10-15 03:16:31 -0600

    add ebv

--- src/config/io.ts
+++ src/config/io.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const url = process.env.API_URL || 'http://localhost:4000'
+const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'
 
-export const socket = io('', { transports: ['websocket'] })
+export const socket = io(url, { transports: ['websocket'] })
